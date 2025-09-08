7ece6e3703afd37f5bd2867602f74a11ea019e9b
igorbeliarmihailov-cmyk <igorbeliarmihailov@gmail.com>
2025-09-08 20:16:11 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ def browser():
     options = Options()
     options.add_argument("--headless")
     browser = webdriver.Edge(options=options)
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--user-data-dir=/home/seluser/selenium")
     browser.maximize_window()
     browser.implicitly_wait(3)
