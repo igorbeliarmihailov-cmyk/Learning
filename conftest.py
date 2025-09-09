09dc798cf0a323fe1a969e7b54a87b89f2c4813b
igorbeliarmihailov-cmyk <igorbeliarmihailov@gmail.com>
2025-09-09 10:51:43 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,8 +6,8 @@ from selenium.webdriver.firefox.options import Options
 @pytest.fixture()
 def browser():
     options = Options()
-    browser = webdriver.Firefox(options=options)
     options.add_argument('--headless')
+    browser = webdriver.Firefox(options=options)
     browser.maximize_window()
     browser.implicitly_wait(3)
     yield browser
