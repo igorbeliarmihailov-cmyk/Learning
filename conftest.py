8c974cb1c1cbeea8353f652d8cf8005aa3803c63
igorbeliarmihailov-cmyk <igorbeliarmihailov@gmail.com>
2025-09-09 08:55:20 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,15 +1,13 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.firefox.options import Options
 
 
 @pytest.fixture()
 def browser():
     options = Options()
     options.add_argument("--headless")
-    browser = webdriver.Edge(options=options)
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--user-data-dir=/home/seluser/selenium")
+    browser = webdriver.Firefox(options=options)
     browser.maximize_window()
     browser.implicitly_wait(3)
     yield browser
